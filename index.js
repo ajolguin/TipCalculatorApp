d4cb4b2fe52cba2d5f25f02a0f8af5ad28a2eb32
Adrian Olguin <aolguin8097@gmail.com>
2022-02-10 18:00:52 -0800

    Latest version -- working on js

---
+++ index.js
@@ -0,0 +1,6 @@
+let calcBtn = document.getElementById("calculateBtn");
+
+calcBtn.addEventListener("click", function () {
+  let amount = document.getElementById("billTotal").value;
+  console.log(amount);
+});
